fd1ef66db4512ea078465bd21585131c07d01465
libinkai <libinkai@bytedance.com>
2020-12-26 09:50:38 +0800

    package

---
+++ src/main/java/com/equator/common/IClient.java
@@ -0,0 +1,8 @@
+package com.equator.common;
+
+/**
+ * @author libinkai
+ * @date 2020/12/25 9:53 上午
+ */
+public interface IClient {
+}
---
+++ src/main/java/com/equator/common/IServer.java
@@ -0,0 +1,9 @@
+package com.equator.common;
+
+/**
+ * @author libinkai
+ * @date 2020/12/24 9:44 下午
+ */
+public interface IServer {
+
+}
